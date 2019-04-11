f17ca44841950d75985bf7cfa9a58954d4e7bdd6
Artem Bobrov <artembobrov3059@gmail.com>
2019-04-11 22:46:11 +0200

    documentation

--- Sources/Table/TableManager/TableManager.swift
+++ Sources/Table/TableManager/TableManager.swift
@@ -3,6 +3,8 @@ import UIKit
 open class TableManager: NSObject, UITableViewDelegate, UITableViewDataSource {
     open weak var tableView: UITableView? {
         didSet {
+			tableView?
+				.dataSource?.tableView(<#T##tableView: UITableView##UITableView#>, cellForRowAt: <#T##IndexPath#>)
             tableView?.dataSource = self
             tableView?.delegate = self
         }
